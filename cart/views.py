4b79aa4c10aec4420077790632ec2428cc24ab5e
FortyTwoFortyTwo <connorrowlanduk@gmail.com>
2021-11-10 14:52:09 +0000

    Added adjust cart and remove from cart views, and calc subtotal

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -26,3 +26,39 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """ Adjust the quantity of the specified product to the specified amount """
+
+    platorm = request.POST.get('platorm')
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id][platorm] = quantity
+    else:
+        del cart[item_id][platorm]
+        if not cart[item_id]:
+            del cart[item_id]
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """ Remove the item from the shopping bag """
+
+    try:
+        platorm = request.POST['platorm']
+        cart = request.session.get('cart', {})
+
+        del cart[item_id][platorm]
+        if not cart[item_id]:
+            del cart[item_id]
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
