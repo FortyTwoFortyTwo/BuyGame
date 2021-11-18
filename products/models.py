b64e787c41058981570dcb86a34fef1301abd09f
FortyTwoFortyTwo <connorrowlanduk@gmail.com>
2021-11-18 18:11:39 +0000

    Finished admin add product functionality

--- products/models.py
+++ products/models.py
@@ -32,7 +32,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    platorms = models.ManyToManyField('Platorm', null=True, blank=True)
+    platorms = models.ManyToManyField('Platorm')
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
